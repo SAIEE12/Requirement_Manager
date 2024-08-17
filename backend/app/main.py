ea152498954187caf393471d3440737393a974ef
Raj P <raj.ml.work@gmail.com>
2024-08-17 15:25:27 +0530

    Basic database connection is established

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import database
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
@@ -9,3 +19,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/db-test")
+def test_db(db: Session = Depends(get_db)):
+    return {"message": "Database connection successful"}
